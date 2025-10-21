e7ec0c7a1c5941c717153bba0ae2decbd04f0f50
Haf <haf@protonmail.ch>
2025-10-21 11:40:25 +0200

    feat: logging for the home manager module

--- packages/wallrotate.nix
+++ packages/wallrotate.nix
@@ -8,7 +8,7 @@ writeShellApplication {
   name = "wallrotate";
 
   runtimeInputs = [
-    coreutils # for `shuf`
+    coreutils # for `shuf` and `date`
     swww
   ];
 
@@ -19,6 +19,7 @@ writeShellApplication {
     while true; do
         IMAGE=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
         swww img "$IMAGE" --transition-type fade
+        echo "$(date --iso-8601=seconds) - Switched to $IMAGE"
         sleep "$INTERVAL"
     done
   '';
